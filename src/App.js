dd9cdf7acf8ae2c454040780a353639c1c373dd0
Manami Ueda <manamiueda1@gmail.com>
2022-10-30 20:02:16 -0400

    set up

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path ='/' element={<Home/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,13 @@
+import './Nav.css'
+
+function Nav() {
+    return (
+      <div className="Nav">
+        <header className="Nav-header">
+
+        </header>
+      </div>
+    );
+  }
+  
+  export default Nav;
